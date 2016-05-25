3ef5f821ff09cc7b64d50bb6c155bdec6e56f758
Calvin Beck <hobbes@ualberta.ca>
2016-05-24 21:08:17 -0600

    Statements and more operators...

--- src/AST.hs
+++ src/AST.hs
@@ -8,6 +8,10 @@ module AST where
 import Text.PrettyPrint.ANSI.Leijen
 
 
+indentLevel :: Int
+indentLevel = 4
+
+
 -- | C expressions with annotations.
 data Expr a = Mult a (Expr a) (Expr a)
             | Div a (Expr a) (Expr a)
@@ -15,10 +19,22 @@ data Expr a = Mult a (Expr a) (Expr a)
             | Add a (Expr a) (Expr a)
             | Sub a (Expr a) (Expr a)
             | Neg a (Expr a)
+            | Pos a (Expr a)
             | IntLit a Integer
             | Ident a String
             | Parens a (Expr a)
             deriving (Show)
+
+
+-- | C statements with annotations.
+data Stmnt a = If a (Expr a) (Stmnt a)
+             | IfElse a (Expr a) (Stmnt a) (Stmnt a)
+             | While a (Expr a) (Stmnt a)
+             | DoWhile (Stmnt a) (Expr a)
+             | For (Expr a) (Expr a) (Expr a) (Stmnt a)
+             | Block a [Stmnt a]
+             | ExprStmnt a (Expr a)
+             deriving (Show)
 
 
 instance Pretty (Expr a) where
@@ -28,6 +44,26 @@ instance Pretty (Expr a) where
   pretty (Add _ a b) = pretty a <+> "+" <+> pretty b
   pretty (Sub _ a b) = pretty a <+> "-" <+> pretty b
   pretty (Neg _ a) = "-" <> pretty a
+  pretty (Pos _ a) = "+" <> pretty a
   pretty (IntLit _ a) = pretty a
   pretty (Ident _ a) = pretty a
   pretty (Parens _ a) = "(" <> pretty a <> ")"
+
+
+instance Pretty (Stmnt a) where
+  pretty (If _ e s) =
+    vcat ["if (" <> pretty e <> ")"
+         , indent indentLevel (pretty s)
+         ]
+  pretty (IfElse _ e s1 s2) =
+    vcat ["if (" <> pretty e <> ")"
+         , indent indentLevel (pretty s1)
+         , "else "
+         , indent indentLevel (pretty s2)
+         ]
+  pretty (ExprStmnt _ e) = pretty e <> ";"
+  pretty (Block _ s) = 
+    vcat [ "{"
+         , indent indentLevel (vcat (map pretty s))
+         , "}" 
+         ]
--- src/Parser.hs
+++ src/Parser.hs
@@ -7,10 +7,10 @@ import Text.Parser.Token.Highlight
 import Text.Parser.Token
 import Text.Parser.Char
 import Text.Trifecta
-import Text.Trifecta.Combinators
 import Control.Applicative
 import qualified Data.HashSet as HS
-
+import Data.Semigroup ((<>))
+import Data.Foldable (asum)
 
 
 -- | Style for C comments.
@@ -51,13 +51,13 @@ cExpression = buildExpressionParser cOperatorTable cTerm
 
 
 cOperatorTable :: (DeltaParsing m, Applicative m) => OperatorTable m (Expr Span)
-cOperatorTable = [ [prefix "-" Neg]  
+cOperatorTable = [ [prefix "-" Neg, prefix "+" Pos]
                  , pure AssocLeft <**> [binary "*" Mult, binary "/" Div, binary "%" Mod]
                  , pure AssocLeft <**> [binary "+" Add, binary "-" Sub]
                  ]
   where binary op f assoc = Infix (spannotate f <$> spanned (reservedOp op)) assoc
         prefix op f = Prefix (spannotate f <$> spanned (reservedOp op))
-        postfix op f = Postfix (spannotate f <$> spanned (reservedOp op))
+        -- postfix op f = Postfix (spannotate f <$> spanned (reservedOp op))
 
 
 cTerm :: DeltaParsing m => m (Expr Span)
@@ -66,12 +66,68 @@ cTerm = ((spannotate1 IntLit <$> spanned natural) <?> "IntLit")
 
 -- | Spanned to an annotation.
 spannotate :: (Span -> b) -> Spanned a -> b
-spannotate f (_ :~ span) = f span
+spannotate f (_ :~ s) = f s
 
 -- | Spanned to an annotation.
 spannotate1 :: (Span -> a -> b) -> Spanned a -> b
-spannotate1 f (a :~ span) = f span a
+spannotate1 f (a :~ s) = f s a
+
+-- | Spanned to annotation.
+spannotate2 :: (Span -> a -> b -> c) -> Spanned a -> Spanned b -> c
+spannotate2 f (a :~ spanA) (b :~ spanB) = f (spanA <> spanB) a b
+
+-- | Spanned to annotation.
+spannotate3 :: (Span -> a -> b -> c -> d) -> Spanned a -> Spanned b -> Spanned c -> d
+spannotate3 f (a :~ spanA) (b :~ spanB) (c :~ spanC) = f (spanA <> spanB <> spanC) a b c
 
 
 reservedOp :: (TokenParsing m, Monad m) => String -> m ()
 reservedOp = reserve emptyOps
+
+
+reserved :: (TokenParsing m, Monad m) => String -> m ()
+reserved = reserve cIdentifierStyle
+
+-- | C statements with annotations.
+{-
+data Stmnt a = If a (Expr a) (Stmnt a)
+             | IfElse a (Expr a) (Stmnt a) (Stmnt a)
+             | While a (Expr a) (Stmnt a)
+             | DoWhile (Stmnt a) (Expr a)
+             | For (Expr a) (Expr a) (Expr a) (Stmnt a)
+             | Block a [Stmnt a]
+             deriving (Show)
+-}
+
+
+cStatement :: (TokenParsing m, DeltaParsing m) => m (Stmnt Span)
+cStatement = 
+  asum [ try (ifElseStatement) <|> ifStatement
+       , expressionStatement
+       , blockStatement
+       ]
+
+
+ifStatement :: (TokenParsing m, DeltaParsing m) => m (Stmnt Span)
+ifStatement = (spannotate2 If)
+    <$> spanned (reserved "if" *> whiteSpace *> parens cExpression <* whiteSpace)
+    <*> spanned (cStatement <* whiteSpace)
+    <?> "if statement"
+
+
+ifElseStatement :: (TokenParsing m, DeltaParsing m) => m (Stmnt Span)
+ifElseStatement = (spannotate3 IfElse)
+    <$> spanned (reserved "if" *> whiteSpace *> parens cExpression <* whiteSpace)
+    <*> spanned (cStatement <* whiteSpace)
+    <*> spanned (reserved "else" *> whiteSpace *> cStatement <* whiteSpace)
+    <?> "if else statement"
+
+
+expressionStatement :: (TokenParsing m, DeltaParsing m) => m (Stmnt Span)
+expressionStatement = spannotate1 ExprStmnt
+    <$> (spanned $ cExpression <* string ";" <* whiteSpace)
+    <?> "expression statement"
+
+
+blockStatement :: (TokenParsing m, DeltaParsing m) => m (Stmnt Span)
+blockStatement = spannotate1 Block <$> (spanned $ braces (many cStatement))
